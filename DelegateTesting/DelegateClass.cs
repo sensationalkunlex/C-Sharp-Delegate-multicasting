5490ad1695b76a348fc51883594535b1194fab47
Kunle <Kunle@DESKTOP-5S3S36H>
2022-06-27 01:07:55 +0100

    Add project files.

---
+++ DelegateTesting/DelegateClass.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class DelegateClass
+{
+    public delegate void delegateDefination(double a, double b);
+    public void Perimeter(double c, double d)
+    {
+        Console.WriteLine($"Perimeter {c + d}");
+    }
+
+    public void Area(double c, double d)
+    {
+        Console.WriteLine($"Area {c * d}");
+    }
+}
---
+++ DelegateTesting/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static DelegateClass;
+
+Console.WriteLine("Hello, World!");
+DelegateClass delegateClass = new DelegateClass();
+
+delegateDefination dele = new delegateDefination(delegateClass.Area);
+dele += delegateClass.Perimeter;
+dele(12, 13);
